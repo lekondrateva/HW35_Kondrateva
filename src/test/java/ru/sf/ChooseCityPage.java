6daf87bdb88eabc55222ffa206c0af003aaf9d36
lekondrateva <umpalus@gmail.com>
2022-01-14 15:39:06 +0500

    изменилось название класса в HTML-структуре главной страницы, заменила Xpath

--- src/test/java/ru/sf/ChooseCityPage.java
+++ src/test/java/ru/sf/ChooseCityPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 
 //Имплементация паттерна Page Object Model
 public record ChooseCityPage(WebDriver webDriver) {
-    private static final String CITY_CHOOSE_FIELD_XPATH = "//span[@class='location-text top-navbar-link ng-tns-c266-2']";
+    private static final String CITY_CHOOSE_FIELD_XPATH = "//span[@class='location-text top-navbar-link ng-tns-c268-2']";
     private static final String CITY_SEARCH_FIELD_XPATH = "//input[@class='input__field' and @id='8']";
     private static final String CITY_NOTFOUND_MESSAGE_SPAN_XPATH = "//p[@class='location-select__location unselectable ng-star-inserted']";
 
